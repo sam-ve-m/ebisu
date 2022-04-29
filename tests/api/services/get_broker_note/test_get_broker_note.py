cd5a6590e11d59c2722b31349465f42c84e88728
renata monteiro <rcm@lionx.com.br>
2022-04-29 16:52:32 -0300

    test broker note pdf

--- tests/api/services/get_broker_note/test_get_broker_note.py
+++ tests/api/services/get_broker_note/test_get_broker_note.py
@@ -1,56 +1,42 @@
 # Standard Libs
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 # External Libs
+from api.repositories.files.repository import FileRepository
 from api.services.get_broker_note.get_broker_note import GetBrokerNotePDF
 from tests.stubs.project_stubs.stub_broker_note_pdf import (
-                                                broker_note_pdf_dummy_br,
-                                                wrong_broker_note_pdf_dummy_br,
-                                                broker_note_link_pdf_dummy,
-                                                file_link_brokerage_dummy)
+    file_link_brokerage_dummy,
+    broker_note_link_dummy,
+    brokerage_note_dummy, broker_note_us_link_dummy)
 from tests.stubs.project_stubs.stub_data import (payload_data_dummy,
-                                                 user_jwt_dummy,
-                                                 portfolios_jwt_dummy,
                                                  StubS3Connection)
 
 
 @pytest.mark.asyncio
-@patch('api.services.get_broker_note.get_broker_note.GetBrokerNotePDF.s3_singleton.generate_file_link',
-       return_value="")
-def test_when_jwt_data_payload_is_valid_then_check_if_the_user_is_in_the_payload_response_for_get_broker_note(
-        mock_get_service_response):
-    response = mock_get_service_response(broker_note_pdf=broker_note_pdf_dummy_br,
-                                         jwt_data=payload_data_dummy)
-    assert response == broker_note_link_pdf_dummy
-    mock_get_service_response.assert_called()
+@patch.object(FileRepository, 'generate_file_link', return_value=broker_note_link_dummy)
+def test_when_jwt_data_payload_is_valid_then_return_the_response_to_get_broker_note(
+        mock_generate_file_link):
+    response = GetBrokerNotePDF.get_service_response(
+        broker_note_pdf=MagicMock(region=MagicMock(value='BR'), year=2021, month=20, day=4),
+        jwt_data=payload_data_dummy)
+    assert response == brokerage_note_dummy
+    mock_generate_file_link.assert_called_once()
 
 
 @pytest.mark.asyncio
-@patch.object(GetBrokerNotePDF, "get_service_response", return_value=broker_note_link_pdf_dummy)
-def test_when_params_and_jwt_are_valid_then_return_the_broker_note_link_as_expected(mock_get_service_response):
-    response = GetBrokerNotePDF.get_service_response(broker_note_pdf=broker_note_pdf_dummy_br,
-                                                     jwt_data=payload_data_dummy)
-    assert response == broker_note_link_pdf_dummy
-    mock_get_service_response.assert_called_once()
+@patch.object(FileRepository, 'generate_file_link', return_value=broker_note_us_link_dummy)
+def test_when_params_are_valid_for_us_then_get_broker_note_link_as_expected(
+        mock_generate_file_link):
+    response = GetBrokerNotePDF.get_service_response(
+        broker_note_pdf=MagicMock(region=MagicMock(value='US'), year=2022, month=10, day=4),
+        jwt_data=payload_data_dummy)
+    assert response == {'pdf_link': broker_note_us_link_dummy}
+    mock_generate_file_link.assert_called_once()
 
 
 @pytest.mark.asyncio
-@patch('api.services.get_broker_note.get_broker_note.GetBrokerNotePDF.get_service_response',
-       return_value=broker_note_link_pdf_dummy)
-def test_when_sending_the_right_params_then_returning_the_pdf_link_to_broker_note(mock_get_service_response):
-    file_link_dummy = 'https://brokerage-note-and-bank-statement.s3.amazonaws.com/109/?AWSAccessKeyId=5243792748DGHDJDH' \
-                      '&signature=FHDGKFU6356489nfhjd65243&expires=875342628946'
-    response = GetBrokerNotePDF.get_service_response(broker_note_pdf=broker_note_pdf_dummy_br,
-                                                     jwt_data=payload_data_dummy)
-    assert 'pdf_link' in response
-    assert response['pdf_link'] == file_link_dummy
-
-
-@pytest.mark.asyncio
-@patch('api.services.get_broker_note.get_broker_note.GetBrokerNotePDF.get_service_response',
-       return_value=broker_note_link_pdf_dummy)
-async def test_when_sending_the_right_file_path_then_return_a_valid_generated_file_link(mock_get_data):
+async def test_when_sending_the_right_file_path_then_return_a_valid_generated_file_link():
     file_path = f"{'49'}/{'BR'}/broker_note/{'2021'}/{'10'}/{'5'}.pdf"
     GetBrokerNotePDF.s3_singleton = StubS3Connection
     response = GetBrokerNotePDF.s3_singleton.generate_file_link(file_path=file_path)
@@ -58,28 +44,26 @@ async def test_when_sending_the_right_file_path_then_return_a_valid_generated_fi
 
 
 @pytest.mark.asyncio
-@patch.object(GetBrokerNotePDF, "get_service_response", return_value=Exception)
-def test_when_broker_note_pydantic_is_missing_any_required_param_then_return_exception(mock_get_service_response):
-    response = GetBrokerNotePDF.get_service_response(broker_note_pdf=wrong_broker_note_pdf_dummy_br,
-                                                     jwt_data=payload_data_dummy)
-    assert response == Exception
+@patch.object(FileRepository, 'generate_file_link', return_value="")
+def test_when_broker_note_pydantic_is_missing_any_required_param_then_return_exception(mock_generate_file_link):
+    with pytest.raises(Exception):
+        response = GetBrokerNotePDF.get_service_response(broker_note_pdf=MagicMock(region=MagicMock(value=''),
+                                                                                   year=None,
+                                                                                   month=20,
+                                                                                   day=4),
+                                                         jwt_data=payload_data_dummy)
+        return response
 
 
 @pytest.mark.asyncio
-@patch.object(GetBrokerNotePDF, "get_service_response", return_value=Exception)
+@patch.object(FileRepository, 'generate_file_link', return_value="")
 def test_when_broker_note_pydantic_and_jwt_is_missing_any_required_param_then_return_exception(
-        mock_get_service_response):
+        mock_generate_file_link):
     payload = {}
-    response = GetBrokerNotePDF.get_service_response(broker_note_pdf=wrong_broker_note_pdf_dummy_br,
-                                                     jwt_data=payload)
-    assert response == Exception
-
-
-@pytest.mark.asyncio
-@patch.object(GetBrokerNotePDF, "get_service_response", return_value=Exception)
-def test_when_jwt_data_payload_is_not_valid_then_return_exception(mock_get_service_response):
-    wrong_payload_data_stub = {}
-    response = GetBrokerNotePDF.get_service_response(broker_note_pdf=broker_note_pdf_dummy_br,
-                                                     jwt_data=wrong_payload_data_stub)
-    assert response == Exception
-    mock_get_service_response.assert_called()
+    with pytest.raises(Exception):
+        response = GetBrokerNotePDF.get_service_response(
+            broker_note_pdf=MagicMock(region=MagicMock(value=''),
+                                      year=None,
+                                      month=20,
+                                      day=4), jwt_data=payload)
+        return response
--- tests/stubs/project_stubs/stub_broker_note_pdf.py
+++ tests/stubs/project_stubs/stub_broker_note_pdf.py
@@ -1,18 +1,12 @@
-broker_note_pdf_dummy_br = {'year': 2021,
-                            'month': 4,
-                            'day': 20,
-                            'region': 'BR'}
 
-wrong_broker_note_pdf_dummy_br = {'year': None,
-                                  'month': 4,
-                                  'day': None,
-                                  'region': 'BR'}
-
-broker_note_link_pdf_dummy = {
-    "pdf_link":
-        "https://brokerage-note-and-bank-statement.s3.amazonaws.com/109/?AWSAccessKeyId=5243792748DGHDJDH&signature="
-        "FHDGKFU6356489nfhjd65243&expires=875342628946"
-}
+brokerage_note_dummy = {'pdf_link': 'https://brokerage-note-and-bank-statement.s3.amazonaws.com/14/Region.BR/'
+                            'broker_note/2022/4/19.pdf?AWSAccessKeyId=AKIATZVFXI25USQWCS5O&Signature='
+                            'U0fZ7EtnQr5c2HStRgLowD8H22w%3D&Expires=1651258518'}
 
 file_link_brokerage_dummy = 'https://brokerage-note-and-bank-statement.s3.amazonaws.com/109/?AWSAccessKeyId=5243792748DGHDJDH' \
                             '&signature=FHDGKFU6356489nfhjd65243&expires=875342628946'
+
+
+broker_note_link_dummy = 'https://brokerage-note-and-bank-statement.s3.amazonaws.com/14/Region.BR/broker_note/2022/4/19.pdf?AWSAccessKeyId=AKIATZVFXI25USQWCS5O&Signature=U0fZ7EtnQr5c2HStRgLowD8H22w%3D&Expires=1651258518'
+
+broker_note_us_link_dummy = "https://brokerage-note-and-bank-statement.s3.amazonaws.com/14/Region.US/broker_note/2022/4/19.pdf?AWSAccessKeyId=AKIATZVFXI25USQWCS5O&Signature=qk787uKUGTTYEFiUomCAZuC0z6w%3D&Expires=1651261099"
