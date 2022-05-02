9dd365f688203bc24655e415f96a85b391c1ae5d
renata monteiro <rcm@lionx.com.br>
2022-05-02 20:56:17 -0300

    test request statement pdf

--- tests/stubs/project_stubs/stub_request_statement_pdf.py
+++ tests/stubs/project_stubs/stub_request_statement_pdf.py
@@ -1,34 +1,3 @@
-# Standard Libs
-import pytest
-from unittest.mock import patch, MagicMock
-
-# External Libs
-from api.repositories.files.repository import FileRepository
-from api.services.request_statement.request_statement import RequestStatement
-from tests.stubs.project_stubs.stub_data import (
-                                                    payload_data_dummy,
-                                                    StubS3Connection)
-
-
-@pytest.mark.asyncio
-def test_generate_path_function_when_sending_the_right_params_then_return_the_expected():
-    response = RequestStatement.generate_path(client_id=None,
-                                              start_date=1646757399000,
-                                              end_date=1648485399000)
-    expected_response = 'None/statements/1646757399000-1648485399000.pdf'
-    assert response == expected_response
-    assert isinstance(response, str)
-
-
-@pytest.mark.asyncio
-def test_when_params_of_generate_path_are_none_then_raise_exception_error():
-    with pytest.raises(Exception) as err:
-        response = RequestStatement.generate_path(  client_id=None,
-                                                    start_date=None,
-                                                    end_date=None)
-        assert err == response
-
-
-@pytest.mark.asyncio
-def test_when_right_params_are_sent_to_generate_pdf_function_then_return_the_expected():
-    pass
+request_bank_statement_pdf_dummy = {
+    "pdf_link": "https://brokerage-note-and-bank-statement.s3.amazonaws.com/None/statements/2022-03-08-2022-03-28.pdf?AWSAccessKeyId=AKIATZVFXI25USQWCS5O&Signature=DS8bqL%2BQ8Lr5AimB6tObvnDNTYY%3D&Expires=1651523423"
+}
