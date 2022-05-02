0cdd6ddc95d7f6c193bd35239df149e6a0211fbd
Marco <msa@lionx.com.br>
2022-05-02 14:16:44 -0300

    gitflow-hotfix-stash: 1.1.4

--- api/services/list_client_orders/strategies/__init__.py
+++ api/services/list_client_orders/strategies/__init__.py
@@ -1,7 +1,8 @@
 from api.services.list_client_orders.strategies.br_orders.strategy import GetBrOrders
 from api.services.list_client_orders.strategies.us_orders.strategy import GetUsOrders
+from api.domain.enums.region import Region
 
 order_region = {
-    "BR": GetBrOrders,
-    "US": GetUsOrders,
+    Region.BR.value: GetBrOrders,
+    Region.US.value: GetUsOrders,
 }
