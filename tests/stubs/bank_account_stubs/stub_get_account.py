0dd0c0e9c351c1b75ac2daca9aaebbbeee7c3768
renata monteiro <rcm@lionx.com.br>
2022-05-04 15:13:32 -0300

    test bank account

---
+++ tests/stubs/bank_account_stubs/stub_get_account.py
@@ -0,0 +1,33 @@
+jwt_data = {
+  "exp": 1650384553,
+  "created_at": 1650383953.299094,
+  "scope": {
+    "user_level": "client",
+    "view_type": "default",
+    "features": [
+      "default"
+    ]
+  },
+  "user": {
+    "unique_id": "2f962bf1-8d29-4363-87a0-0e9640b6e7b5",
+    "nick_name": "RAST3",
+    "portfolios": {
+      "br": {
+        "bovespa_account": "000000211-4",
+        "bmf_account": "211"
+      },
+      "us": {
+        "dw_account": None
+      }
+    },
+    "client_has_br_trade_allowed": False,
+    "client_has_us_trade_allowed": False,
+    "client_profile": "investor"
+  }}
+
+
+jwt_data_mist = {
+      "unique_id": "2f962bf1-8d29-4363-87a0-0e9640b6e7b5",
+      "device_identifier": "df9cf8cd-2bff-4c52-b7ac-123b2064cd3e",
+      "exp": 1650805214
+    }
