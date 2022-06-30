618a4035203ead8b61d7bcf07972873d6d1d6ec3
renata monteiro <rcm@lionx.com.br>
2022-06-30 17:43:32 -0300

    adding transport layer drive wealth tests

---
+++ tests/src/transport/drive_wealth/earnings/stub_transport.py
@@ -0,0 +1,30 @@
+# PROJECT IMPORTS
+from src.domain.date_formatters.region.date_time.model import RegionStringDateTime
+from src.domain.date_formatters.region.enum.date_format.enum import RegionDateFormat
+from src.domain.earning.base.model.earning.model import Earning
+
+
+get_transactions_stub = [{"dividend": {"type": "CASH",
+               "amountPerShare": 0.1511,
+               "taxCode": "FULLY_TAXABLE"
+               }, "instrument": {"id": "9563a550-9f3b-47aa-b570-f18435f218d2",
+               "symbol": "AAPL",
+               "name": "PowerShares S&P 500 High Div Low Vol ETF"
+               }, "tranWhen": "2019-06-03T15:12:00.345Z", }]
+
+earnings_transaction_stub = {"dividend": {"type": "CASH",
+               "amountPerShare": 0.1511,
+               "taxCode": "FULLY_TAXABLE"
+               }, "instrument": {"id": "9563a550-9f3b-47aa-b570-f18435f218d2",
+               "symbol": "AAPL",
+               "name": "PowerShares S&P 500 High Div Low Vol ETF"
+               }, "tranWhen": "2019-06-03T15:12:00.345Z", }
+
+get_transactions_model_stub = Earning(**{
+            "symbol": "AAPL",
+            "name": "PowerShares S&P 500 High Div Low Vol ETF",
+            "amount_per_share":  0.1511,
+            "type": "CASH",
+            "tax_code": "FULLY_TAXABLE",
+            "date": RegionStringDateTime("2019-06-03T15:12:00.345Z",
+                                         RegionDateFormat.US_DATE_FORMAT)})
--- tests/src/transport/drive_wealth/earnings/test_transport.py
+++ tests/src/transport/drive_wealth/earnings/test_transport.py
@@ -1,5 +1,6 @@
 # STANDARD IMPORTS
 from unittest.mock import patch
+from datetime import datetime
 import pytest
 
 # PROJECT IMPORTS
@@ -7,7 +8,7 @@ from src.domain.date_formatters.region.enum.date_format.enum import RegionDateFo
 from src.domain.date_formatters.region.timestamp.model import RegionTimeStamp
 from src.domain.statement.us.request.model import TransactionRequest, QueryParams
 from src.transport.drive_wealth.earnings.transport import DwEarningsTransport
-from tests.src.services.earnings_from_client.stub_earnings import dw_earnings_example
+from tests.src.transport.drive_wealth.earnings.stub_transport import get_transactions_model_stub, get_transactions_stub
 
 
 class Iterable:
@@ -15,41 +16,44 @@ class Iterable:
         return self
 
 
-get_transactions_stub = [{"dividend": {"type": "CASH",
-                    "amountPerShare": 0.1511,
-                    "taxCode": "FULLY_TAXABLE"
-                },"instrument": {"id": "9563a550-9f3b-47aa-b570-f18435f218d2",
-                    "symbol": "SPHD",
-                    "name": "PowerShares S&P 500 High Div Low Vol ETF"
-                },"tranWhen": "2019-06-03T15:12:00.345Z",},
-                 {"dividend": {"type": "CASH",
-               "amountPerShare": 0.1511,
-               "taxCode": "FULLY_TAXABLE"
-               }, "instrument": {"id": "9563a550-9f3b-47aa-b570-f18435f218d2",
-               "symbol": "SPHD",
-               "name": "PowerShares S&P 500 High Div Low Vol ETF"
-               }, "tranWhen": "2019-06-03T15:12:00.345Z", }]
-
-
 @pytest.mark.asyncio
-@patch.object(DwEarningsTransport, "_DwEarningsTransport__get_transactions", return_value=dw_earnings_example)
-@patch.object(DwEarningsTransport, "_DwEarningsTransport__build_earning_model", return_value="")
+@patch.object(DwEarningsTransport, "_DwEarningsTransport__get_transactions", return_value=get_transactions_stub)
+@patch.object(DwEarningsTransport, "_DwEarningsTransport__build_earning_model", side_effect=[get_transactions_model_stub, {}])
 async def test_get_us_transaction_earnings_when_sending_right_params_then_return_the_expected(
-        mock_get_transactions
+        mock_get_transactions, mock_build_earning_model
 ):
     Iterable.__next__ = mock_get_transactions
     response = await DwEarningsTransport.get_us_transaction_earnings(
         transaction_request=TransactionRequest(
             account='89c69304-018a-40b7-be5b-2121c16e109e.1651525277006',
-            query_params=QueryParams(limit=1,
-                                     offset=RegionTimeStamp(
-                                         1642699893000, region_date_format=RegionDateFormat.US_DATE_FORMAT),
-                                     from_date=RegionTimeStamp(
-                                         1646757399000, region_date_format=RegionDateFormat.US_DATE_FORMAT),
-                                     to_date=RegionTimeStamp(
-                                         1648485399000, region_date_format=RegionDateFormat.US_DATE_FORMAT)
-                                     )
+            query_params=QueryParams(
+                limit=1,
+                offset=RegionTimeStamp(
+                     1642699893000, region_date_format=RegionDateFormat.US_DATE_FORMAT),
+                from_date=RegionTimeStamp(
+                     1646757399000, region_date_format=RegionDateFormat.US_DATE_FORMAT),
+                to_date=RegionTimeStamp(
+                     1648485399000, region_date_format=RegionDateFormat.US_DATE_FORMAT)
+                )
         )
     )
 
-    assert response == ""
+    assert response == [get_transactions_model_stub]
+    assert isinstance(response, list)
+
+
+@pytest.mark.asyncio
+async def test_get_us_transaction_earnings_when_sending_wrong_params_then_return_the_expected():
+    with pytest.raises(AttributeError):
+        await DwEarningsTransport.get_us_transaction_earnings(
+            transaction_request=TransactionRequest(
+                account=None,
+                query_params=QueryParams(
+                    limit=1,
+                    offset=None,
+                    from_date=None,
+                    to_date=RegionTimeStamp(
+                        1648485399000, region_date_format=RegionDateFormat.US_DATE_FORMAT)
+                )
+            )
+        )
