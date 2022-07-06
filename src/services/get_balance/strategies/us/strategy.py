90cb972735dcbf3d5950cc5362bd4f69b70a0bfb
Marco Sievers de Almeida <msa@lionx.com.br>
2022-07-06 09:53:04 -0300

    feat: add balance

--- src/services/get_balance/strategies/us/strategy.py
+++ src/services/get_balance/strategies/us/strategy.py
@@ -1,8 +1,13 @@
-from src.services.statement.service import Statement
-
-
 class GetUsBalance:
-    @staticmethod
-    async def get_balance(account: str) -> str:
-        balance_us = await Statement.get_dw_balance(account)
+    dw = DWTransport()
+
+    @classmethod
+    async def get_balance(cls, account: str) -> str:
+        balance_us = await cls.get_dw_balance(account)
         return balance_us
+
+    @staticmethod
+    async def get_dw_balance(dw_account: str):
+        raw_balance = await Statement.dw.get_balances(dw_account)
+        balance = Statement.normalize_balance_us(*raw_balance)
+        return {"balance": balance}
