c0a2b69824da8de2fc5820bbb5503951b6ce2818
Igor Magro <ilm@lionx.com.br>
2022-08-18 18:12:38 -0300

    chore: change orders_list and order_details queries

--- src/services/get_client_orders/strategies/br_orders/strategy.py
+++ src/services/get_client_orders/strategies/br_orders/strategy.py
@@ -9,10 +9,10 @@ class GetBrOrdersDetails:
     @staticmethod
     def build_query(accounts: List[str], clordid: str) -> str:
         query = f"""SELECT C.MESSAGE, A.*
-                    FROM USOLUDB001.VW_HIERARCHY_EXECUTION_REPORTS A
+                    FROM UORDEDB001.VW_ORDER_DETAILS A
                     LEFT JOIN UORDEDB001.MAP_ORDERS_REJECT_REASON_TO_SIGAME_MESSAGES B ON A.ORDREJREASON = B.B3_MESSAGE_ID
                     LEFT JOIN UORDEDB001.SIGAME_ORDERS_MESSAGES C ON B.SIGAME_MESSAGE_ID = C.SIGAME_MESSAGE_ID
-                    WHERE NODE_ACCOUNT in ('{"', '".join(accounts)}') AND NODE_CLORDID = '{clordid}'
+                    WHERE ACCOUNT in ('{"', '".join(accounts)}') AND ROOTCLORDID = '{clordid}'
                     ORDER BY CREATEDAT DESC
                     """
         return query
