53a703f598c901496fba8d8392f31e097db2e57b
Vinicius Reis <vinicius.reis@lionx.com.br>
2022-09-08 18:43:06 -0300

    refactoring forex exchange service

--- src/services/forex_exchange/proposal_simulation/service.py
+++ src/services/forex_exchange/proposal_simulation/service.py
@@ -1,20 +1,27 @@
 # Ebisu
+from src.domain.exceptions.domain.forex_exchange.exception import ErrorOnValidateExchangeSimulationProposalData
 from src.domain.exceptions.service.forex_exchange.exception import (
-    UserTokenNotFound, InvalidOperation, ErrorOnGetUserToken, ErrorOnGetExchangeSimulationProposal
+    CustomerQuotationTokenNotFound, ErrorOnGetCustomerQuotationToken, ErrorOnGetExchangeSimulationProposal,
 )
-from src.domain.validators.forex_exchange.currency_options import CurrencyExchange
+from src.domain.exceptions.repository.exception import CustomerExchangeDataNotFound
 from src.domain.models.forex_exchange.customer_exchange_request_data.model import CustomerExchangeResquestModel
+from src.domain.models.forex_exchange.customer_exchange_response_data.model import CustomerExchangeResponseModel
+from src.domain.validators.forex_exchange.currency_options import CurrencyExchange
+
 from src.repositories.user.repository import UserRepository
 from caronte import AllowedHTTPMethods, ExchangeCompanyApi
 
 # Standards
 from typing import Union
 
+# Third party
+from etria_logger import Gladsheim
 
-class ProposalSimulationService:
+
+class CustomerExchangeService:
 
     @classmethod
-    async def get_exchange_proposal_simulation(cls, jwt_data: dict, currency_exchange: CurrencyExchange):
+    async def get_proposal_simulation(cls, jwt_data: dict, currency_exchange: CurrencyExchange):
         exchange_account_id = jwt_data.get("user", {}).get("exchange_account_id", 208785)
         customer_exchange_data = await cls.__get_customer_exchange_account_data(
             exchange_account_id=exchange_account_id, currency_exchange=currency_exchange
@@ -26,27 +33,41 @@ class ProposalSimulationService:
         customer_token = await cls.__get_customer_token_on_route_21(
             exchange_proposal_model=exchange_proposal_model
         )
-        exchange_simulation_proposal_data = await cls.__get_exchange_proposal_values_on_route_22(
+        exchange_simulation_proposal_data = await cls.__get_exchange_simulation_proposal_data_on_route_22(
             customer_token=customer_token,
         )
-        response = await cls.__treatment_exchange_simulation_data(exchange_simulation_proposal_data=exchange_simulation_proposal_data)
+        exchange_simulation_proposal_response = await cls.__treatment_and_validation_exchange_simulation_data(
+            exchange_simulation_proposal_data=exchange_simulation_proposal_data
+        )
+
+        return exchange_simulation_proposal_response
 
     @staticmethod
-    async def __treatment_exchange_simulation_data(exchange_simulation_proposal_data: dict):
-
+    async def __treatment_and_validation_exchange_simulation_data(exchange_simulation_proposal_data: dict) -> dict:
+        try:
+            exchange_simulation_model = await CustomerExchangeResponseModel.get_customer_exchange_model(
+                exchange_simulation_proposal_data=exchange_simulation_proposal_data
+            )
+            exchange_simulation_proposal_response = {
+                "exchange_simulation_proposal": exchange_simulation_model.dict()
+            }
+            return exchange_simulation_proposal_response
+        except Exception as ex:
+            Gladsheim.error(error=ex)
+            raise ErrorOnValidateExchangeSimulationProposalData()
 
     @staticmethod
-    async def __get_customer_exchange_account_data(exchange_account_id: int, currency_exchange: CurrencyExchange) -> Union[InvalidOperation, dict]:
+    async def __get_customer_exchange_account_data(exchange_account_id: int, currency_exchange: CurrencyExchange) -> Union[CustomerExchangeDataNotFound, dict]:
         customer_exchange_data = await UserRepository.get_user_exchange_data(
             exchange_account_id=exchange_account_id,
             base=currency_exchange.base,
             quote=currency_exchange.quote)
         if not customer_exchange_data:
-            raise InvalidOperation()
+            raise CustomerExchangeDataNotFound()
         return customer_exchange_data
 
     @staticmethod
-    async def __get_customer_token_on_route_21(exchange_proposal_model: CustomerExchangeResquestModel) -> Union[str, UserTokenNotFound, ErrorOnGetUserToken]:
+    async def __get_customer_token_on_route_21(exchange_proposal_model: CustomerExchangeResquestModel) -> Union[str, CustomerQuotationTokenNotFound, ErrorOnGetCustomerQuotationToken]:
         url_path = await exchange_proposal_model.build_url_path_to_request_current_currency_quote()
         success, caronte_status, content = await ExchangeCompanyApi.request_as_client(
             method=AllowedHTTPMethods.GET,
@@ -54,14 +75,14 @@ class ProposalSimulationService:
             exchange_account_id=exchange_proposal_model.exchange_account_id
         )
         if not success:
-            raise ErrorOnGetUserToken()
+            raise ErrorOnGetCustomerQuotationToken()
         customer_token = content.get("token")
         if not customer_token:
-            raise UserTokenNotFound()
+            raise CustomerQuotationTokenNotFound()
         return customer_token
 
     @staticmethod
-    async def __get_exchange_simulation_proposal_on_route_22(customer_token: str, exchange_proposal_model: CustomerExchangeResquestModel) -> Union[dict, ErrorOnGetExchangeSimulationProposal]:
+    async def __get_exchange_simulation_proposal_data_on_route_22(customer_token: str, exchange_proposal_model: CustomerExchangeResquestModel) -> Union[dict, ErrorOnGetExchangeSimulationProposal]:
         url_path = await exchange_proposal_model.get_url_path_to_request_exchange_simulation()
         body = await exchange_proposal_model.get_body_template_to_request_exchange_simulation(customer_token=customer_token)
         success, caronte_status, exchange_simulation_proposal_data = ExchangeCompanyApi.request_as_client(
@@ -73,15 +94,3 @@ class ProposalSimulationService:
         if not success:
             raise ErrorOnGetExchangeSimulationProposal()
         return exchange_simulation_proposal_data
-
-
-if __name__ == '__main__':
-    import asyncio
-
-    async def abc():
-        url = "https://sbxapi.ourinvest.com.br:43400/api/v1/produto/cambio/naturezaOperacao/4/taxa/BRL/USD/cliente/208785/spread/0.02"
-        success, caronte_status, content = await ExchangeCompanyApi.request_as_client(method=AllowedHTTPMethods.GET, url=url, exchange_account_id=208785)
-        print(content)
-        print(content.get('token'))
-
-    a = asyncio.run(abc())
