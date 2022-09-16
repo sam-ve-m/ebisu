3603b341af0f865c39d501457f3affac8a4c979d
Nivaldo <nj@lionx.com.br>
2022-09-16 09:30:02 -0300

    feat: mist-token adicionado mo endpoint create_bank_account

--- src/routers/funding_and_withdrawal/router.py
+++ src/routers/funding_and_withdrawal/router.py
@@ -21,24 +21,13 @@ class FundingAndWithdrawalRouter:
         return FundingAndWithdrawalRouter.__funding_and_withdrawal_router
 
     @staticmethod
-    async def get_jwt_data_and_validate_electronica_signature(request: Request):
-
-        jwt_data = await JwtService.get_thebes_answer_from_request(request=request)
-        valid_electronica_signature = await JwtService.validate_electronic_signature(
-            request, user_data=jwt_data["user"]
-        )
-        if not valid_electronica_signature:
-            raise InvalidElectronicaSignature()
-        return jwt_data
-
-    @staticmethod
     @__funding_and_withdrawal_router.post(
         "/user/money_flow_between_user_accounts_in_same_exchange",
         tags=["User Funding And Withdrawal"],
     )
     async def add_funding(request: Request, user_money_flow: UserMoneyFlowSameExchange):
 
-        jwt_data = await FundingAndWithdrawalRouter.get_jwt_data_and_validate_electronica_signature(
+        jwt_data = await JwtService.get_jwt_data_and_validate_electronica_signature(
             request=request
         )
         get_user_bank_accounts_response = (
@@ -63,7 +52,7 @@ class FundingAndWithdrawalRouter:
         request: Request, user_money_flow: UserMoneyFlowDifferentExchange
     ):
 
-        jwt_data = await FundingAndWithdrawalRouter.get_jwt_data_and_validate_electronica_signature(
+        jwt_data = await JwtService.get_jwt_data_and_validate_electronica_signature(
             request=request
         )
         get_user_bank_accounts_response = (
@@ -88,7 +77,7 @@ class FundingAndWithdrawalRouter:
         request: Request, user_withdrawal: UserMoneyFlowToExternalBank
     ):
 
-        jwt_data = await FundingAndWithdrawalRouter.get_jwt_data_and_validate_electronica_signature(
+        jwt_data = await JwtService.get_jwt_data_and_validate_electronica_signature(
             request=request
         )
         get_user_bank_accounts_response = (
