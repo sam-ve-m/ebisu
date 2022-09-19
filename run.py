ff88a9dddd3b56c09d61e8dd1762159c70d867d3
Nivaldo <nj@lionx.com.br>
2022-09-19 11:22:30 -0300

    feat_update

--- run.py
+++ run.py
@@ -9,8 +9,5 @@ from src.routers.base_router import BaseRouter
 
 app = BaseRouter.register_routers()
 
-from unittest.mock import patch
-from persephone_client import Persephone, PersephoneClientStatus
-with patch.object(Persephone, "send_to_persephone", return_value=(True, PersephoneClientStatus.success)):
-    if __name__ == "__main__":
-        uvicorn.run(app, host="0.0.0.0", port=8000, log_config=GLADSHEIM_LOGGING_CONFIG)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_config=GLADSHEIM_LOGGING_CONFIG)
