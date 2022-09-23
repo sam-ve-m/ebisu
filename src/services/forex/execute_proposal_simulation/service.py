de1d5b85546deb97e57b203afd6e3d36f88fe697
Vinicius Reis <vinicius.reis@lionx.com.br>
2022-09-23 17:33:57 -0300

    wip: rename jwt ouroinvest service

--- src/services/forex/execute_proposal_simulation/service.py
+++ src/services/forex/execute_proposal_simulation/service.py
@@ -1,11 +1,26 @@
 from src.domain.validators.forex.execution_proposal import ForexExecution
-from src.services.forex.jwt.service import JwtService
+from src.domain.models.forex.forex_proposal.execution.model import ExecutionProposalModel
+from src.services.forex.jwt_ouroinvest.service import JwtService
 from caronte import ExchangeCompanyApi
+from halberd import (
+    BalanceLockManagerService,
+    Resource,
+    Country as HalberdCountry,
+    LockBalanceStatus
+)
+
 
 class ExecutionExchangeService:
 
     @classmethod
     async def execute_exchange_proposal(cls, payload: ForexExecution, jwt_data: dict):
-        token_decoded = JwtService.decode_jwt_token(jwt_data=payload.customer_proposal_token)
-        execution_proposal_model =
+        token_decoded = await JwtService.decode_jwt_token(jwt_data=payload.customer_proposal_token)
+        execution_proposal_model = ExecutionProposalModel(
+            jwt_data=jwt_data,
+            token_decoded=token_decoded,
+            payload=payload
+        )
         pass
+
+    @classmethod
+    async def check_customer_has_enough_balance(cls):
