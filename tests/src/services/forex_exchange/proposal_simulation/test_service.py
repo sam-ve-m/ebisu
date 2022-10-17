3e019d192a91c2949c5b5b991d042e6552c1d176
Vinicius Reis <vinicius.reis@lionx.com.br>
2022-10-16 23:47:26 -0300

    tests fixes

--- tests/src/services/forex_exchange/proposal_simulation/test_service.py
+++ tests/src/services/forex_exchange/proposal_simulation/test_service.py
@@ -12,9 +12,9 @@ from src.domain.exceptions.service.forex.exception import (
     CustomerQuotationTokenNotFound,
 )
 
-from src.services.forex.proposal.simulation import (
-    CustomerExchangeService,
-)
+from src.services.forex.account.service import ForexAccount
+from src.services.forex.proposal.simulation.service import ForexSimulation
+
 
 # Stubs
 from .stubs import (
@@ -23,7 +23,7 @@ from .stubs import (
     stub_caronte_response_success_21,
     stub_caronte_response_forbidden,
     stub_caronte_response_bad_request,
-    stub_customer_exchange_request_model,
+    stub_simulation_model,
     stub_config_path_quotation,
     stub_caronte_response_success_22,
     stub_caronte_response_token_not_found,
@@ -45,45 +45,45 @@ import pytest
 
 @pytest.mark.asyncio
 @patch(
-    "src.services.forex.simulation.service.UserRepository.get_user_exchange_data",
+    "src.services.forex.proposal.simulation.service.UserExchangeRepository.get_spread_data",
     return_value=stub_customer_exchange_data,
 )
 async def test_when_customer_have_exchange_data_then_return_expected_values(
     mock_user_repository,
 ):
-    result = await CustomerExchangeService._CustomerExchangeService__get_customer_exchange_account_data(
-        exchange_account_id=12345, currency_exchange=stub_currency_exchange
+    result = await ForexSimulation._ForexSimulation__get_customer_spread_by_operation_type(
+        account_number=12345, payload=stub_currency_exchange
     )
     assert isinstance(result, dict)
-    assert result.get("exchange_account_id") == 12345
+    assert result.get("account_number") == 12345
 
 
 @pytest.mark.asyncio
 @patch(
-    "src.services.forex.simulation.service.UserRepository.get_user_exchange_data",
+    "src.services.forex.proposal.simulation.service.UserExchangeRepository.get_spread_data",
     return_value=None,
 )
 async def test_when_customer_not_have_exchange_data_then_raises(mock_user_repository):
     with pytest.raises(CustomerForexDataNotFound):
-        await CustomerExchangeService._CustomerExchangeService__get_customer_exchange_account_data(
-            exchange_account_id=12345, currency_exchange=stub_currency_exchange
+        await ForexSimulation._ForexSimulation__get_customer_spread_by_operation_type(
+            account_number=12345, payload=stub_currency_exchange
         )
 
 
 @pytest.mark.asyncio
 @patch(
-    "src.domain.models.forex.simulation_request_data.model.config",
+    "src.domain.models.forex.proposal.simulation_request_data.model.config",
     side_effect=stub_config_path_quotation,
 )
 @patch(
-    "src.services.forex.simulation.service.ExchangeCompanyApi.request_as_client",
+    "src.services.forex.proposal.simulation.service.ExchangeCompanyApi.request_as_client",
     return_value=stub_caronte_response_success_21,
 )
 async def test_when_get_customer_token_with_successfully_then_return_token_data(
     mock_request_caronte, mock_config
 ):
-    result = await CustomerExchangeService._CustomerExchangeService__get_customer_token_on_route_21(
-        customer_exchange_request_model=stub_customer_exchange_request_model
+    result = await ForexSimulation._ForexSimulation__get_customer_token_on_route_21(
+        simulation_model=stub_simulation_model
     )
 
     assert isinstance(result, dict)
@@ -91,109 +91,109 @@ async def test_when_get_customer_token_with_successfully_then_return_token_data(
 
 @pytest.mark.asyncio
 @patch(
-    "src.domain.models.forex.simulation_request_data.model.config",
+    "src.domain.models.forex.proposal.simulation_request_data.model.config",
     side_effect=stub_config_path_quotation,
 )
 @patch(
-    "src.services.forex.simulation.service.ExchangeCompanyApi.request_as_client",
+    "src.services.forex.proposal.simulation.service.ExchangeCompanyApi.request_as_client",
     return_value=stub_caronte_response_bad_request,
 )
 async def test_when_get_customer_token_result_is_bad_request_then_raises(
     mock_request_caronte, mock_config
 ):
     with pytest.raises(ExpiredToken):
-        await CustomerExchangeService._CustomerExchangeService__get_customer_token_on_route_21(
-            customer_exchange_request_model=stub_customer_exchange_request_model
+        await ForexSimulation._ForexSimulation__get_customer_token_on_route_21(
+            simulation_model=stub_simulation_model
         )
 
 
 @pytest.mark.asyncio
 @patch(
-    "src.domain.models.forex.simulation_request_data.model.config",
+    "src.domain.models.forex.proposal.simulation_request_data.model.config",
     side_effect=stub_config_path_quotation,
 )
 @patch(
-    "src.services.forex.simulation.service.ExchangeCompanyApi.request_as_client",
+    "src.services.forex.proposal.simulation.service.ExchangeCompanyApi.request_as_client",
     return_value=stub_caronte_response_unauthorized,
 )
 async def test_when_get_customer_token_result_is_unauthorized_then_raises(
     mock_request_caronte, mock_config
 ):
     with pytest.raises(InvalidToken):
-        await CustomerExchangeService._CustomerExchangeService__get_customer_token_on_route_21(
-            customer_exchange_request_model=stub_customer_exchange_request_model
+        await ForexSimulation._ForexSimulation__get_customer_token_on_route_21(
+            simulation_model=stub_simulation_model
         )
 
 
 @pytest.mark.asyncio
 @patch(
-    "src.domain.models.forex.simulation_request_data.model.config",
+    "src.domain.models.forex.proposal.simulation_request_data.model.config",
     side_effect=stub_config_path_quotation,
 )
 @patch(
-    "src.services.forex.simulation.service.ExchangeCompanyApi.request_as_client",
+    "src.services.forex.proposal.simulation.service.ExchangeCompanyApi.request_as_client",
     return_value=stub_caronte_response_forbidden,
 )
 async def test_when_get_customer_token_result_is_forbidden_then_raises(
     mock_request_caronte, mock_config
 ):
     with pytest.raises(DroppedToken):
-        await CustomerExchangeService._CustomerExchangeService__get_customer_token_on_route_21(
-            customer_exchange_request_model=stub_customer_exchange_request_model
+        await ForexSimulation._ForexSimulation__get_customer_token_on_route_21(
+            simulation_model=stub_simulation_model
         )
 
 
 @pytest.mark.asyncio
 @patch(
-    "src.domain.models.forex.simulation_request_data.model.config",
+    "src.domain.models.forex.proposal.simulation_request_data.model.config",
     side_effect=stub_config_path_quotation,
 )
 @patch(
-    "src.services.forex.simulation.service.ExchangeCompanyApi.request_as_client",
+    "src.services.forex.proposal.simulation.service.ExchangeCompanyApi.request_as_client",
     return_value=stub_caronte_response_token_not_found,
 )
 async def test_when_get_customer_token_result_is_token_not_found_then_raises(
     mock_request_caronte, mock_config
 ):
     with pytest.raises(CaronteCantFindToken):
-        await CustomerExchangeService._CustomerExchangeService__get_customer_token_on_route_21(
-            customer_exchange_request_model=stub_customer_exchange_request_model
+        await ForexSimulation._ForexSimulation__get_customer_token_on_route_21(
+            simulation_model=stub_simulation_model
         )
 
 
 @pytest.mark.asyncio
 @patch(
-    "src.domain.models.forex.simulation_request_data.model.config",
+    "src.domain.models.forex.proposal.simulation_request_data.model.config",
     side_effect=stub_config_path_quotation,
 )
 @patch(
-    "src.services.forex.simulation.service.ExchangeCompanyApi.request_as_client",
+    "src.services.forex.proposal.simulation.service.ExchangeCompanyApi.request_as_client",
     return_value=stub_caronte_response_unexpected_error,
 )
 async def test_when_get_customer_token_result_is_unexpected_error_found_then_raises(
     mock_request_caronte, mock_config
 ):
     with pytest.raises(UnexpectedErrorInExchangeAPI):
-        await CustomerExchangeService._CustomerExchangeService__get_customer_token_on_route_21(
-            customer_exchange_request_model=stub_customer_exchange_request_model
+        await ForexSimulation._ForexSimulation__get_customer_token_on_route_21(
+            simulation_model=stub_simulation_model
         )
 
 
 @pytest.mark.asyncio
 @patch(
-    "src.domain.models.forex.simulation_request_data.model.config",
+    "src.domain.models.forex.proposal.simulation_request_data.model.config",
     side_effect=stub_config_path_exchange_simulation,
 )
 @patch(
-    "src.services.forex.simulation.service.ExchangeCompanyApi.request_as_client",
+    "src.services.forex.proposal.simulation.service.ExchangeCompanyApi.request_as_client",
     return_value=stub_caronte_response_success_22,
 )
 async def test_when_get_exchange_simulation_with_success_then_return_exchange_simulation_data(
     mock_request_caronte, mock_config
 ):
-    result = await CustomerExchangeService._CustomerExchangeService__get_exchange_simulation_proposal_data_on_route_22(
+    result = await ForexSimulation._ForexSimulation__get_exchange_simulation_proposal_data_on_route_22(
         customer_token=stub_token_generated_in_route_21,
-        customer_exchange_request_model=stub_customer_exchange_request_model,
+        simulation_model=stub_simulation_model,
     )
 
     assert isinstance(result, dict)
@@ -201,102 +201,102 @@ async def test_when_get_exchange_simulation_with_success_then_return_exchange_si
 
 @pytest.mark.asyncio
 @patch(
-    "src.domain.models.forex.simulation_request_data.model.config",
+    "src.domain.models.forex.proposal.simulation_request_data.model.config",
     side_effect=stub_config_path_exchange_simulation,
 )
 @patch(
-    "src.services.forex.simulation.service.ExchangeCompanyApi.request_as_client",
+    "src.services.forex.proposal.simulation.service.ExchangeCompanyApi.request_as_client",
     return_value=stub_caronte_response_bad_request,
 )
 async def test_when_get_exchange_simulation_result_is_bad_request_then_raises(
     mock_request_caronte, mock_config
 ):
     with pytest.raises(ExpiredToken):
-        await CustomerExchangeService._CustomerExchangeService__get_exchange_simulation_proposal_data_on_route_22(
+        await ForexSimulation._ForexSimulation__get_exchange_simulation_proposal_data_on_route_22(
             customer_token=stub_token_generated_in_route_21,
-            customer_exchange_request_model=stub_customer_exchange_request_model,
+            simulation_model=stub_simulation_model,
         )
 
 
 @pytest.mark.asyncio
 @patch(
-    "src.domain.models.forex.simulation_request_data.model.config",
+    "src.domain.models.forex.proposal.simulation_request_data.model.config",
     side_effect=stub_config_path_exchange_simulation,
 )
 @patch(
-    "src.services.forex.simulation.service.ExchangeCompanyApi.request_as_client",
+    "src.services.forex.proposal.simulation.service.ExchangeCompanyApi.request_as_client",
     return_value=stub_caronte_response_forbidden,
 )
 async def test_when_get_exchange_simulation_result_is_forbidden_then_raises(
     mock_request_caronte, mock_config
 ):
     with pytest.raises(DroppedToken):
-        await CustomerExchangeService._CustomerExchangeService__get_exchange_simulation_proposal_data_on_route_22(
+        await ForexSimulation._ForexSimulation__get_exchange_simulation_proposal_data_on_route_22(
             customer_token=stub_token_generated_in_route_21,
-            customer_exchange_request_model=stub_customer_exchange_request_model,
+            simulation_model=stub_simulation_model,
         )
 
 
 @pytest.mark.asyncio
 @patch(
-    "src.domain.models.forex.simulation_request_data.model.config",
+    "src.domain.models.forex.proposal.simulation_request_data.model.config",
     side_effect=stub_config_path_exchange_simulation,
 )
 @patch(
-    "src.services.forex.simulation.service.ExchangeCompanyApi.request_as_client",
+    "src.services.forex.proposal.simulation.service.ExchangeCompanyApi.request_as_client",
     return_value=stub_caronte_response_unauthorized,
 )
 async def test_when_get_exchange_simulation_result_is_unauthorized_then_raises(
     mock_request_caronte, mock_config
 ):
     with pytest.raises(InvalidToken):
-        await CustomerExchangeService._CustomerExchangeService__get_exchange_simulation_proposal_data_on_route_22(
+        await ForexSimulation._ForexSimulation__get_exchange_simulation_proposal_data_on_route_22(
             customer_token=stub_token_generated_in_route_21,
-            customer_exchange_request_model=stub_customer_exchange_request_model,
+            simulation_model=stub_simulation_model,
         )
 
 
 @pytest.mark.asyncio
 @patch(
-    "src.domain.models.forex.simulation_request_data.model.config",
+    "src.domain.models.forex.proposal.simulation_request_data.model.config",
     side_effect=stub_config_path_exchange_simulation,
 )
 @patch(
-    "src.services.forex.simulation.service.ExchangeCompanyApi.request_as_client",
+    "src.services.forex.proposal.simulation.service.ExchangeCompanyApi.request_as_client",
     return_value=stub_caronte_response_token_not_found,
 )
 async def test_when_get_exchange_simulation_result_is_token_not_found_then_raises(
     mock_request_caronte, mock_config
 ):
     with pytest.raises(CaronteCantFindToken):
-        await CustomerExchangeService._CustomerExchangeService__get_exchange_simulation_proposal_data_on_route_22(
+        await ForexSimulation._ForexSimulation__get_exchange_simulation_proposal_data_on_route_22(
             customer_token=stub_token_generated_in_route_21,
-            customer_exchange_request_model=stub_customer_exchange_request_model,
+            simulation_model=stub_simulation_model,
         )
 
 
 @pytest.mark.asyncio
 @patch(
-    "src.domain.models.forex.simulation_request_data.model.config",
+    "src.domain.models.forex.proposal.simulation_request_data.model.config",
     side_effect=stub_config_path_exchange_simulation,
 )
 @patch(
-    "src.services.forex.simulation.service.ExchangeCompanyApi.request_as_client",
+    "src.services.forex.proposal.simulation.service.ExchangeCompanyApi.request_as_client",
     return_value=stub_caronte_response_unexpected_error,
 )
 async def test_when_get_exchange_simulation_result_is_unexpected_error_then_raises(
     mock_request_caronte, mock_config
 ):
     with pytest.raises(UnexpectedErrorInExchangeAPI):
-        await CustomerExchangeService._CustomerExchangeService__get_exchange_simulation_proposal_data_on_route_22(
+        await ForexSimulation._ForexSimulation__get_exchange_simulation_proposal_data_on_route_22(
             customer_token=stub_token_generated_in_route_21,
-            customer_exchange_request_model=stub_customer_exchange_request_model,
+            simulation_model=stub_simulation_model,
         )
 
 
 @pytest.mark.asyncio
 async def test_when_have_token_data_then_return_customer_token_data():
-    result = await CustomerExchangeService._CustomerExchangeService__validate_if_token_exists_in_content(
+    result = await ForexSimulation._ForexSimulation__validate_if_token_exists_in_content(
         content=stub_response_rote_21
     )
 
@@ -306,14 +306,14 @@ async def test_when_have_token_data_then_return_customer_token_data():
 @pytest.mark.asyncio
 async def test_when_have_token_data_then_return_customer_token_data():
     with pytest.raises(CustomerQuotationTokenNotFound):
-        await CustomerExchangeService._CustomerExchangeService__validate_if_token_exists_in_content(
+        await ForexSimulation._ForexSimulation__validate_if_token_exists_in_content(
             content={}
         )
 
 
 @pytest.mark.asyncio
 async def test_when_treatment_and_validation_with_success_then_return_exchange_simulation_data():
-    result = await CustomerExchangeService._CustomerExchangeService__treatment_and_validation_exchange_simulation_data(
+    result = await ForexSimulation._ForexSimulation__treatment_and_validation_exchange_simulation_data(
         exchange_simulation_proposal_data=stub_response_rote_22
     )
 
@@ -323,32 +323,34 @@ async def test_when_treatment_and_validation_with_success_then_return_exchange_s
 @pytest.mark.asyncio
 async def test_when_treatment_and_validation_failed_then_raises():
     with pytest.raises(ErrorValidatingSimulationProposalData):
-        await CustomerExchangeService._CustomerExchangeService__treatment_and_validation_exchange_simulation_data(
+        await ForexSimulation._ForexSimulation__treatment_and_validation_exchange_simulation_data(
             exchange_simulation_proposal_data=stub_response_missing_data_rote_22
         )
 
 
 @pytest.mark.asyncio
 @patch.object(
-    CustomerExchangeService,
-    "_CustomerExchangeService__get_exchange_simulation_proposal_data_on_route_22",
+    ForexSimulation,
+    "_ForexSimulation__get_exchange_simulation_proposal_data_on_route_22",
     return_value=stub_response_rote_22,
 )
 @patch.object(
-    CustomerExchangeService,
-    "_CustomerExchangeService__get_customer_token_on_route_21",
+    ForexSimulation,
+    "_ForexSimulation__get_customer_token_on_route_21",
     return_value=stub_response_rote_21,
 )
 @patch.object(
-    CustomerExchangeService,
-    "_CustomerExchangeService__get_customer_exchange_account_data",
+    ForexSimulation,
+    "_ForexSimulation__get_customer_spread_by_operation_type",
     return_value=stub_customer_exchange_data,
 )
+@patch.object(ForexAccount, "get_account_number", return_value=67890)
+@patch.object(ForexAccount, "get_client_id", return_value=12345)
 async def test_when_get_exchange_proposal_with_success_then_return_validated_exchange_simulation_proposal(
-    mock_data, mock_route_21, mock_route_22
+    mock_cliend_id, mock_account_number, mock_data, mock_route_21, mock_route_22
 ):
-    result = await CustomerExchangeService.get_proposal_simulation(
-        jwt_data={}, currency_exchange=stub_currency_exchange
+    result = await ForexSimulation.get_proposal_simulation(
+        jwt_data={}, payload=stub_currency_exchange
     )
 
     assert isinstance(result, dict)
